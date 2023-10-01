8a2b08600896ec3c72249421fb6d5d88ed338656
Ramin Karimi <raminkarimi1378@gmail.com>
2023-10-01 13:23:24 +0330

    migrate SongsScreen to new MusicalRemote

--- app/src/main/java/rk/musical/ui/screen/SongsScreenViewModel.kt
+++ app/src/main/java/rk/musical/ui/screen/SongsScreenViewModel.kt
@@ -1,51 +1,61 @@
 package rk.musical.ui.screen
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.launch
+import rk.musical.data.SongRepository
 import rk.musical.data.model.Song
-import rk.musical.data.model.toMediaItems
-import rk.musical.data.model.toSongs
-import rk.musical.player.MusicalRemoteControl
+import rk.musical.player.MusicalRemote
 import javax.inject.Inject
 
 @HiltViewModel
 class SongsScreenViewModel @Inject constructor(
-    private val remoteControl: MusicalRemoteControl,
+    private val songRepository: SongRepository,
+    private val musicalRemote: MusicalRemote
 ) : ViewModel() {
     var uiState: SongsScreenUiState by mutableStateOf(SongsScreenUiState.Empty)
         private set
 
-    fun startToCollectSongs() {
+    private var currentSongs = emptyList<Song>()
+    private var hasCurrentPlaylist = false
+
+    init {
+        Log.i(SongsScreenViewModel::class.simpleName, "Created ViewModel")
         viewModelScope.launch {
-            remoteControl.browserEvent.collectLatest {
-                if (it is MusicalRemoteControl.BrowserEvent.Connected)
-                    loadSongs()
-            }
+            uiState = SongsScreenUiState.Loading
+            songRepository.localSongs
+                .stateIn(scope = viewModelScope)
+                .collect {
+                    uiState = if (it.isEmpty())
+                        SongsScreenUiState.Empty
+                    else {
+                        currentSongs = it
+                        SongsScreenUiState.Loaded(it)
+                    }
+                }
         }
     }
 
-
-    private suspend fun loadSongs() {
-        uiState = SongsScreenUiState.Loading
-        val songs = remoteControl.getSongsMediaItems()?.toSongs()
-        remoteControl.setPlaylist(songs?.toMediaItems() ?: emptyList())
-        uiState = if (songs == null) {
-            SongsScreenUiState.Empty
-        } else {
-            SongsScreenUiState.Loaded(songs)
+    fun refreshSongs() {
+        viewModelScope.launch {
+            songRepository.loadSongs()
         }
-    }
 
+    }
 
     fun playSong(index: Int) {
-        remoteControl.playSongFromIndex(index)
+        if (!hasCurrentPlaylist) {
+            musicalRemote.setPlaylist(currentSongs)
+            hasCurrentPlaylist = true
+        }
+        musicalRemote.playSong(index)
     }
 
 
